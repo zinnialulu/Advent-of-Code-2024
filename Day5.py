aa2325b16adf795793da0e1ac4bc935562416a33
zinnialulu <49621708+zinnialulu@users.noreply.github.com>
2024-12-05 16:15:27 +0000

    clean a bit of codes and comments

--- Day5.py
+++ Day5.py
@@ -1,6 +1,6 @@
-#find instruction section
+#find instruction or rule section
 #find page section
-file=open('day5.txt','r')
+file= open('day5.txt','r')
 input=file.read()
 
 allinput=input.split('\n\n')
@@ -10,87 +10,65 @@ rule=allinput[0].split('\n')
 instruction=allinput[1].split('\n')
 #print(instruction)
 
+
+###---Part I---###
+#each xx|yy is a pattern, make a list of violation of pattern with yy|xx
+#if there is any violation then it is incorrect = if none of violation found, it is correct
+
 import re
 
 violation=[]
+wrong_instruction=[]
 for item in rule:
-    p1=item.split('|')[1]+".*?"+item.split('|')[0]
-    #print(p1)
-    violation.append(p1)
-#print(violation)
+    p=item.split('|')[1]+".*?"+item.split('|')[0]
+    violation.append(p) #form a list of all violation rules
 
 sum=0
-wrong_instruction=[]
 for eachinstruction in instruction:
-    for i in violation:
-        if bool(re.search(i,eachinstruction))==True:
-            #print(f"violated: {eachinstruction}")
-            check="incorrect"
+    for i in violation: #check if each violation rule appeared
+        if bool(re.search(i,eachinstruction))==True: 
             wrong_instruction.append(eachinstruction)
+            check="incorrect"
             break
         else:
-            #find middle page:
             check="correct"
     if check=="correct":
         newlist=eachinstruction.split(',')
         l=len(newlist)
-        #print(eachinstruction)
-        index=(l)//2
-        #print(type(index))
-        #print(f"correct: {eachinstruction}")
-        if newlist[index]!= '':
-            num=int(newlist[index])
-            #print(num)
-            sum+=num
-print(f"part1 answer: {sum}")  
+        index=l//2 #find index of middle number
+        num=int(newlist[index])
+        sum+=num
+print(f"part 1 answer: {sum}")
+#find the middle number of each 'correct' page instruction
 
 
+###--Part II---###
 
-
-###---Part I---###
-
-#each xx|yy is a pattern, make a list of violation of pattern which is yy|xx
-#if there is any violation pattern found in the page instruction then it is incorrect = if none of violation found, it is correct
-
-#find the middle page of all correct page instruction
-
-
-
+#try to find a way to re=arrange each wrong instruction
+#option 1: when you find the violation, reverse the numbers then continue
+#option 2: magic sorting function => cmp
 import functools
-###---Part II---###
-#try to find a way to re-arrange wrong instruction
-#option1: when you find the violation, reverse the numbers then begin from beginning? then continue
-#option2: euhhhhh magic sorting?
-#print(wrong_instruction)
-#print(rule)
+
 patterns=[]
 for item in rule:
-    p=(item.split('|')[0],item.split('|')[1])
-    #print(p1)
+    p=(item.split('|')[0],item.split('|')[1]) #it needs to be a tuple
     patterns.append(p)
-#print(patterns)
-#s=['75','97','47','61','53']
 
-wrong_gen = (line.split(',') for line in wrong_instruction)
-def compare_rule(n1,n2):
+wrong_gen=(line.split(',') for line in wrong_instruction) #create a generator type for sorted()
+
+def compare(n1,n2): #define a function to be used in cmp_to_key
     if (n1,n2) in patterns:
         return -1
-    elif (n1,n2) in patterns:
+    elif (n2,n1) in patterns:
         return 1
     else:
         return 0
 
 sum_2=0
-
 for x in wrong_gen:
-    a=sorted(x, key = functools.cmp_to_key(compare_rule))
+    a=sorted(x,key=functools.cmp_to_key(compare)) 
     l=len(a)
-        #print(eachinstruction)
-    index=(l)//2
-    if a[index]!= '':
-            mid=int(a[index])
-            #print(num)
-            sum_2+=mid
-    #print(f"s is {s}")
-    #print(f"a is {a}")
-print(f"part2 answer: {sum_2}")
+    index=l//2
+    mid=int(a[index])
+    sum_2+=mid
+print(f"part 2 answer: {sum_2}")
