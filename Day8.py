90c8b748b40833bd3168eddbb47f18f3e8a98ee7
zinnialulu <49621708+zinnialulu@users.noreply.github.com>
2024-12-09 12:17:20 +0000

    remove test file for Day7 and add Day8 part 1 which needs to be corrected

--- Day8.py
+++ Day8.py
@@ -1,5 +1,5 @@
 ###---Part I---###
-file=open('test3.txt','r')
+file=open('day8.txt','r')
 input=file.read()
 
 matrix=[]
@@ -8,43 +8,43 @@ for line in input.splitlines():
     for i in range(len(line)):
         line_list.append(line[i])
     matrix.append(line_list)
-print(matrix)
+#print(matrix)
 
 #each case, if it is letter or number (x1,y1), find another same one at (x2,y2)
 #the "#" will be at (2x1-x2,2y1-y2),(2x2-x1,2y2-y1) if it is not a letter nor a number
 
 
-l1="".join(line for line in input.splitlines())
-print(l1)
-
+single_line="".join(line for line in input.splitlines())
+line_list=list(single_line)
+#print(l1)
 
 import re
 
-ch="A"
-print(re.search(ch,l1))
-
-for match in re.finditer(ch,l1):
-    print(match.span())
-    (start,end)=match.span()
-    print(start)
-    print(end)
-    col=start%12 #12=len(matrix[0])
-    row=start//12
-    print(row,col)
-
-
-    
-    
-
-#p="[a-Z]|[0-9]"
-
-# for row in range(len(matrix)):
-#     for col in range(len(matrix[0])):
-#         if re.match(p,matrix[row][col]
-
-
-
-
-
-
-#find all "#"
+antenna=[]
+pos=[]
+for i in range(len(single_line)):
+    if (single_line[i]!="."): #find all antenna
+        pat=single_line[i]
+        antenna.append(pat)
+antenna=set(antenna)
+print(f"antenna is {antenna}")
+for item in antenna:       
+    item_pos=[]
+    for match in re.finditer(item,single_line):
+        start,end=match.span()
+        item_pos.append(start) #generate all cordinates of 
+    print(f"antenna {item} is found at {item_pos}")
+    for i in range(len(item_pos)-1):
+        for j in range(i+1,len(item_pos)):
+            x1=int(item_pos[i])
+            x2=int(item_pos[j])
+            new_left_x=2*x1-x2
+            if new_left_x in range(len(single_line)):
+                pos.append(new_left_x)
+            new_right_x=2*x2-x1
+            if new_right_x in range(len(single_line)):
+                pos.append(new_right_x)
+                
+print(f"antinode for all antenna found at {pos}")               
+cnt=len(set(pos)) #find all "#"
+print(cnt)
